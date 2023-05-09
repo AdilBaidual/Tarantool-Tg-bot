9f10e0668fa1154e165e44c60ea6e52affc14278
AdilBaidual <baidual.adil@yandex.ru>
2023-05-09 15:55:52 +0300

    auto delete add

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+
 import telebot
 import sqlite3
 
 bot = telebot.TeleBot('6019984780:AAE5EXixznM7MEVyc3xQT_YPXnZ9X06Iugs')
+
 
 @bot.message_handler(commands=['start'])
 def satrt(message):
@@ -15,10 +18,15 @@ def satrt(message):
     cursor.close()
     connection.close()
 
+
 @bot.message_handler(commands=['set'])
 def set_handler(message):
-    bot.send_message(message.chat.id, 'Введите login:')
-    bot.register_next_step_handler(message, set_login)
+    if check_db():
+        bot.send_message(message.chat.id, 'Введите login:')
+        bot.register_next_step_handler(message, set_login)
+    else:
+        bot.send_message(message.chat.id, 'Введите /start')
+
 
 def set_login(message):
     user_id = message.from_user.id
@@ -26,18 +34,21 @@ def set_login(message):
     bot.send_message(message.chat.id, 'Введите password:')
     bot.register_next_step_handler(message, set_password, user_id, login)
 
+
 def set_password(message, user_id, login):
     password = message.text
+    bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     bot.send_message(message.chat.id, 'Введите service:')
     bot.register_next_step_handler(message, set_service, user_id, login, password)
 
+
 def set_service(message, user_id, login, password):
     service = message.text
-
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO users(user_id, login, password, service) VALUES (?, ?, ?, ?)', (user_id, login, password, service))
+    cursor.execute('INSERT INTO users(user_id, login, password, service) VALUES (?, ?, ?, ?)',
+                   (user_id, login, password, service))
     connection.commit()
 
     cursor.close()
@@ -45,11 +56,15 @@ def set_service(message, user_id, login, password):
 
     bot.send_message(message.chat.id, 'Данные сохранены')
 
+
 @bot.message_handler(commands=['get'])
 def get_handler(message):
-    # запрашиваем у пользователя сервис
-    bot.send_message(message.chat.id, "Введите сервис:")
-    bot.register_next_step_handler(message, get_service)
+    if check_db():
+        bot.send_message(message.chat.id, "Введите сервис:")
+        bot.register_next_step_handler(message, get_service)
+    else:
+        bot.send_message(message.chat.id, 'Введите /start')
+
 
 def get_service(message):
     service = message.text
@@ -60,16 +75,23 @@ def get_service(message):
     result = cursor.fetchone()
     if result:
         login, password = result
-        bot.send_message(message.chat.id, f"Логин: {login}\nПароль: {password}")
+        tmp = bot.send_message(message.chat.id, f"Логин: {login}\nПароль: {password}")
+        time.sleep(5)
+        bot.delete_message(chat_id=message.chat.id, message_id=tmp.message_id)
     else:
         bot.send_message(message.chat.id, "Данные не найдены!")
     cursor.close()
     connection.close()
 
+
 @bot.message_handler(commands=['del'])
 def delete_service(message):
-    bot.send_message(message.chat.id, 'Введите service:')
-    bot.register_next_step_handler(message, delete, message.from_user.id)
+    if check_db():
+        bot.send_message(message.chat.id, 'Введите service:')
+        bot.register_next_step_handler(message, delete, message.from_user.id)
+    else:
+        bot.send_message(message.chat.id, 'Введите /start')
+
 
 def delete(message, user_id):
     service = message.text
@@ -85,6 +107,16 @@ def delete(message, user_id):
         bot.reply_to(message, f"Запись для сервиса '{service}' не найдена.")
     cursor.close()
     connection.close()
+
+
+def check_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users'")
+    table_exists = cursor.fetchone() is not None
+    cursor.close()
+    conn.close()
+    return table_exists
 
 
 bot.polling(none_stop=True)
